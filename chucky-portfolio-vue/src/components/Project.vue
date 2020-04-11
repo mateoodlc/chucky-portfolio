e2a3d4eef68fdadef5df924da64bf98f644d531c
Mateo Orobio <mateo.orobio@idfive.com>
2020-04-11 01:54:59 -0500

    feat: Project component created

---
+++ chucky-portfolio-vue/src/components/Project.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script>
+export default {
+  data() {
+    return {
+      key: value,
+      quote: document.documentElement.classList.add('holi')
+    }
+  }
+}
+</script>
+
+<style></style>
--- chucky-portfolio-vue/src/views/Home.vue
+++ chucky-portfolio-vue/src/views/Home.vue
@@ -1,18 +1,30 @@
+<style lang="scss" src="../styles/home.scss" scoped></style>
 <template>
-  <div class="home">
+  <div class="home__main-container">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="home__hero-wrapper">
+      <div class="home__hero-info__wrapper">
+        <h1>Aura Bravo.</h1>
+        <p>
+          Hi, my name is Aura Bravo. I'm an Industrial Designer passionate about
+          digital media and interfaces, it's design and development. Here is
+          some of my work.
+        </p>
+      </div>
+      <div class="home__hero-image__wrapper"></div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Project from '../components/Project.vue'
+//import styles from '../styles/home.scss'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
+    Project
   }
-};
+}
 </script>
